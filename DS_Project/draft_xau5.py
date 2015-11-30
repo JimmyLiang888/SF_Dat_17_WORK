c1aecbd2f27172ea8b9d4f1f63ad1b072a919dfa
Jimmy Liang <Jimmy.Liang888@gmail.com>
2015-11-30 12:35:23 -0800

    update latest file

--- DS_Project/draft_xau5.py
+++ DS_Project/draft_xau5.py
@@ -335,9 +335,31 @@ XAU_data.corr()
 # display correlation matrix in Seaborn using a heatmap
 sns.heatmap(XAU_data.corr())
 
+'''
 # Need to edit variables to match up with data sets...
 # Holt-Winters Exponentially weighted moving averages smoothing
 ewma = pandas.stats.moments.ewma
+# make a hat function, and add noise
+x = np.linspace(0,1,100)
+x = np.hstack((x,x[::-1]))
+x += np.random.normal( loc=0, scale=0.1, size=200 )
+plot( x, alpha=0.4, label='Raw' )
+ 
+# take EWMA in both directions with a smaller span term
+fwd = ewma( x, span=15 ) # take EWMA in fwd direction
+bwd = ewma( x[::-1], span=15 ) # take EWMA in bwd direction
+c = np.vstack(( fwd, bwd[::-1] )) # lump fwd and bwd together
+c = np.mean( c, axis=0 ) # average
+ 
+# regular EWMA, with bias against trend
+plot( ewma( x, span=20 ), 'b', label='EWMA, span=20' )
+ 
+# "corrected" (?) EWMA
+plot( c, 'r', label='Reversed-Recombined' )
+ 
+legend(loc=8)
+savefig( 'ewma_correction.png', fmt='png', dpi=100 )
+
 
 def holt_winters_second_order_ewma( x, span, beta ):
     N = x.size
@@ -369,7 +391,7 @@ plot( c, 'r', label='Reversed-Recombined' )
  
 legend(loc=8)
 savefig( 'ewma_correction.png', fmt='png', dpi=100 )
-
+'''
 
 ################################
 #
